cfe45dcdc6e449ecacbfd61aea1ae9ae7add1e3a
Lijing Lin <lijinglin29@gmail.com>
2026-01-21 11:34:33 +0800

    Committing 1 item from workspace 00f16074-feb2-4e8c-af25-067818e6bc56

---
+++ test02/Notebook_1_1.Notebook/notebook-content.py
@@ -0,0 +1,30 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "454e23fe-707c-4c15-8bd2-d753d530f536",
+# META       "default_lakehouse_name": "yilinlh2024",
+# META       "default_lakehouse_workspace_id": "d0af6ece-b6fe-4d05-a679-f85a757950a6"
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+1 + 1
+2 + 2
+3 + 3
+4 + 4
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
