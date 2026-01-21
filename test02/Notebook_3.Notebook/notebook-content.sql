d87d1355c12908d7d334b2f01b57a336731d2b59
Lijing Lin <lijinglin29@gmail.com>
2026-01-21 12:55:34 +0800

    Committing 1 item from workspace 00f16074-feb2-4e8c-af25-067818e6bc56

---
+++ test02/Notebook_3.Notebook/notebook-content.sql
@@ -0,0 +1,31 @@
+-- Fabric notebook source
+
+-- METADATA ********************
+
+-- META {
+-- META   "kernel_info": {
+-- META     "name": "sqldatawarehouse"
+-- META   },
+-- META   "dependencies": {
+-- META     "warehouse": {
+-- META       "default_warehouse": "f5783003-286b-ac4e-4a71-e105b4874fda",
+-- META       "known_warehouses": [
+-- META         {
+-- META           "id": "f5783003-286b-ac4e-4a71-e105b4874fda",
+-- META           "type": "Datawarehouse"
+-- META         }
+-- META       ]
+-- META     }
+-- META   }
+-- META }
+
+-- CELL ********************
+
+select * from table1
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sql",
+-- META   "language_group": "sqldatawarehouse"
+-- META }
